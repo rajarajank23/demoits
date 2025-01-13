e6fec0cf624eca0748d8e1d05609eb759e7449c9
rajarajank23 <rajarajank23@gmail.com>
2025-01-14 02:12:55 +0530

    sdsd

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock-table"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
+terraform {
+  backend "s3" {
+    bucket         = "your-s3-bucket-name"
+    key            = "terraform/statefile/terraform.tfstate"
+    region         = "us-east-1" # Replace with your region
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
   }
 }
 
