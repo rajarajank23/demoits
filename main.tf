be84846cb3417e567993648452513fd4045a167a
rajarajank23 <rajarajank23@gmail.com>
2025-01-14 02:09:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
- terraform {
-  backend "s3" {
-    bucket         = "raja"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-	dynamodb_table = "terraform-state-lock"
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
 
