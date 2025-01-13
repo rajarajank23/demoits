8db292c577c3507b1a020a9120f4a7145a5c4fb1
rajarajank23 <rajarajank23@gmail.com>
2025-01-14 01:56:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-/*terraform {
+ terraform {
   backend "s3" {
     bucket         = "raja"
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
 	dynamodb_table = "terraform-state-lock"
   }
-}*/
+}
 
 provider "aws" {
   region = "us-east-1"
