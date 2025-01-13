1e553a238af7847357a208be6a1ca70da5ef6ffa
rajarajank23 <rajarajank23@gmail.com>
2025-01-14 01:54:14 +0530

    yes

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 /*terraform {
   backend "s3" {
     bucket         = "raja"
-    key            = "key/terraform.tfstate"
+    key            = "state/terraform.tfstate"
     region         = "us-east-1"
+	dynamodb_table = "terraform-state-lock"
   }
 }*/
 
